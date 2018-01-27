5f109df1f3f4e4c1c89824be2f7d06943e3fffbd
Kirill <kirill99.tkachev@yandex.by>
2018-01-27 16:47:15 +0300

    unt commit

--- app/src/main/java/com/example/Adapters/BusListAdapter.java
+++ app/src/main/java/com/example/Adapters/BusListAdapter.java
@@ -30,11 +30,10 @@ import com.example.kirill.stopping.R;
         String text = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NUMBER));
         String type = cursor.getString(cursor.getColumnIndex("type"));
         textView.setText(text);
-        if(type.equals("A")){
-            imageView.setImageResource(R.drawable.busnormal);
-        }else{
+        if (type.equals("A")) {
+            imageView.setImageResdfdfdfdource(R.drawable.busnormal);
+        } else {
             imageView.setImageResource(R.drawable.busexpress);
-
         }
     }
 }
