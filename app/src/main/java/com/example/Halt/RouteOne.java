0ccaf9990ada1be2e057039eea88122204fb1a1d
Kirill <kirill99.tkachev@yandex.by>
2018-01-30 23:27:39 +0300

    resolve NearTransportAdapter

--- app/src/main/java/com/example/Halt/RouteOne.java
+++ app/src/main/java/com/example/Halt/RouteOne.java
@@ -151,7 +151,7 @@ public class RouteOne extends Fragment {
 
     private List<String> getBusNamber(String numb){
         List<String> number = new ArrayList<>();
-        for(int i = 0; i<idCursor.getCount()-1;i++){
+        for(int i = 0; i<idCursor.getCount();i++){
             number.add(numb);
         }
         return number;
