d0f676d08ea0e1874b89da78546b364d725139b4
KirillTK <kirill99.tkachev@yandex.by>
2018-03-20 14:12:21 +0300

    доделал вкладку маршрута

--- app/src/main/java/com/example/Map/MapActivity.java
+++ app/src/main/java/com/example/Map/MapActivity.java
@@ -1,18 +1,38 @@
 package com.example.Map;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.Halt.Halt;
 import com.example.kirill.stopping.R;
 
 public class MapActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        String key = getIntent().getExtras().getString("number");
+        getSupportActionBar().setTitle(key);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/Map/mapViewer.java
+++ app/src/main/java/com/example/Map/mapViewer.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
