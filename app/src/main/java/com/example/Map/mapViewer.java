838593d239e402d6a97f97facf02db181432d1f0
Kirill <kirill99.tkachev@yandex.by>
2018-05-02 01:09:00 +0300

    Маркер не добавляется на уже существующем на карте

--- app/src/main/java/com/example/Map/mapViewer.java
+++ app/src/main/java/com/example/Map/mapViewer.java
@@ -9,7 +9,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,16 +23,12 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import java.util.ArrayList;
-import static com.example.Map.busStopsDatabase.ShapeID;
-import static com.example.Map.busStopsDatabase.tableName;
-import static com.example.Map.busStopsDatabase.tableNameRoutes;
-import static com.example.Map.busStopsDatabase.tableNameShapes;
-import static com.example.kirill.stopping.Bus_fragment.number;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class mapViewer extends Fragment {
@@ -42,6 +37,10 @@ public class mapViewer extends Fragment {
     Cursor userCursor;
     busStopsDatabase dataBaseConnection;
     SQLiteDatabase database;
+    private double a1;
+    private double a2;
+    private double b1;
+    private double b2;
 
     public mapViewer() {
 
@@ -70,7 +69,7 @@ public class mapViewer extends Fragment {
                 mMap.setMinZoomPreference(11);
                 LatLng centralPoint = new LatLng(53.90308663, 27.56050229);
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centralPoint, 11));
-                LatLngBounds ADELAIDE = new LatLngBounds(new LatLng(53.83206766, 27.36127853), new LatLng(53.98153138, 27.69086838));
+                LatLngBounds ADELAIDE = new LatLngBounds(new LatLng(53.795195,  27.265686), new LatLng(53.997308, 27.829322));
                 mMap.setLatLngBoundsForCameraTarget(ADELAIDE);
                 if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET}
@@ -97,6 +96,7 @@ public class mapViewer extends Fragment {
                         String type = getActivity().getIntent().getStringExtra("type");
                         userCursor = database.rawQuery("SELECT * FROM Routes WHERE transport = '"+number+"' and type = '"+type+"'",null);
                         userCursor.moveToFirst();
+                        String STOPS = userCursor.getString(userCursor.getColumnIndex("STOPS"));
                         String shapeID = userCursor.getString(userCursor.getColumnIndex("ROUTEID"));
                         userCursor = database.rawQuery("SELECT * FROM Shapes WHERE ID = "+shapeID+";",null); ///Находит шифр
                         ArrayList<LatLng> PolyLine = new ArrayList<LatLng>();
@@ -108,18 +108,23 @@ public class mapViewer extends Fragment {
                                 mMap.addMarker(new MarkerOptions().position(new LatLng(PolyLine.get(i).latitude, PolyLine.get(i).longitude)).icon(
                                         BitmapDescriptorFactory
                                                 .defaultMarker(BitmapDescriptorFactory.HUE_RED)).title("A"));
+                                a1 =  PolyLine.get(i).latitude;
+                                a2 =  PolyLine.get(i).longitude;
+                                Log.d("location1","latitude "+a1+"  longitude "+a2);
                             }
                             if (i == PolyLine.size() - 1 ){
                                 mMap.addMarker(new MarkerOptions().position(new LatLng(PolyLine.get(i).latitude, PolyLine.get(i).longitude)).icon(
                                         BitmapDescriptorFactory
                                                 .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).title("B"));
+                                b1 = PolyLine.get(i).latitude;
+                                b2 = PolyLine.get(i).longitude;
                             }
                             ruta.add(new LatLng(PolyLine.get(i).latitude, PolyLine.get(i).longitude));
                         }
                         ruta.color(Color.RED).width(7);
                         Polyline polygon = mMap.addPolyline(ruta);
 
-                        if (zoomLevel>=16)
+                        if (zoomLevel>=13)
                         {
                             LatLngBounds currentRectangle = mMap.getProjection().getVisibleRegion().latLngBounds;
                             userCursor = database.rawQuery("SELECT * FROM Coordinates WHERE LONG > " +
@@ -129,8 +134,12 @@ public class mapViewer extends Fragment {
                                     currentRectangle.northeast.longitude,null);
                             for (userCursor.moveToFirst(); !userCursor.isAfterLast(); userCursor.moveToNext())
                             {
-                                LatLng busStopToDraw = new LatLng(userCursor.getDouble(userCursor.getColumnIndex(busStopsDatabase.stopLat)),userCursor.getDouble(userCursor.getColumnIndex(busStopsDatabase.stopLong)));
-                                mMap.addMarker(new MarkerOptions().position(busStopToDraw).title(userCursor.getString(userCursor.getColumnIndex(busStopsDatabase.stopName))));
+                                if (isCurrectTransportLocation(STOPS,userCursor.getString(0)) && chekMarker(a1,a2,userCursor.getDouble(3),userCursor.getDouble(2)) && chekMarker(b1,b2,userCursor.getDouble(3),userCursor.getDouble(2)) )
+                                {
+                                    Log.d("В цикле location1","latitude "+userCursor.getString(3)+"  longitude "+userCursor.getString(2));
+                                    LatLng busStopToDraw = new LatLng(userCursor.getDouble(userCursor.getColumnIndex(busStopsDatabase.stopLat)), userCursor.getDouble(userCursor.getColumnIndex(busStopsDatabase.stopLong)));
+                                    mMap.addMarker(new MarkerOptions().position(busStopToDraw).icon(BitmapDescriptorFactory.fromResource(R.drawable.stop1)).title(userCursor.getString(userCursor.getColumnIndex(busStopsDatabase.stopName))));
+                                }
                             }
                         }
                     }
@@ -138,6 +147,20 @@ public class mapViewer extends Fragment {
             }
         });
         return rootView;
+    }
+
+    public boolean chekMarker(double a1, double a2 , double b1 , double b2){
+        double c1 =  Math.abs(a1 - b1);
+        double c2 = Math.abs(a2 - b2);
+        if(c1 >0.0005 && c2>0.0005){
+            return true;
+        }else return false;
+    }
+
+    public boolean isCurrectTransportLocation(String STOPS , String ID){
+        Pattern pattern = Pattern.compile(ID);
+        Matcher matcher = pattern.matcher(STOPS);
+        return  matcher.find();
     }
 
     @Override
